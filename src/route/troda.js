95891681993fd152625934fa4177661efd492e45
Hans Kristian Flaatten <hans@starefossen.com>
2014-02-07 22:49:42 +0100

    Add Troda API endpoints

---
+++ src/route/troda.js
@@ -0,0 +1,15 @@
+exports.getList = function(req, res, next) {
+  res.json({foo: 'bar'});
+};
+
+exports.post = function(req, res, next) {
+  res.json({foo: 'bar'});
+};
+
+exports.get = function(req, res, next) {
+  res.json({foo: 'bar'});
+};
+
+exports.postTask = function(req, res, next) {
+  res.json({foo: 'bar'});
+};
--- src/server.js
+++ src/server.js
@@ -21,5 +21,11 @@ app.get('/api/finn/:source/', function(req, res, next){
   }
 });
 
+var troda = require('./route/troda');
+
+app.post('/api/troda/', troda.getList);
+app.get('/api/troda/', troda.post);
+app.get('/api/troda/:id', troda.get);
+app.post('/api/troda/:id/task', troda.postTask);
 
 app.listen(process.env.PORT_WWW || 8080);
