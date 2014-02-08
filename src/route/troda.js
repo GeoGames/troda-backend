cdaa61139ab4d4a7491cfc4235b81120545ac9d7
Hans Kristian Flaatten <hans@starefossen.com>
2014-02-08 01:06:11 +0100

    Implement GET /api/troda

--- src/route/troda.js
+++ src/route/troda.js
@@ -1,7 +1,10 @@
 var db = require('./../db/mongo');
 
 exports.getList = function(req, res, next) {
-  res.json({foo: 'bar'});
+  db.troda.find().toArray(function(err, docs) {
+    if (err) return next(err);
+    res.json(docs);
+  });
 };
 
 exports.post = function(req, res, next) {
