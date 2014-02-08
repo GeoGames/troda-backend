21943b9fb05fa9197ae05b90ed878cd956319fe4
Ali Kaafarani <ali@kvikshaug.no>
2014-02-08 04:55:04 +0100

    Add missing semicolon

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
-}
+};
 
 if (!module.parent) { app.use(express.logger('dev')); }
 app.use(express.urlencoded());
